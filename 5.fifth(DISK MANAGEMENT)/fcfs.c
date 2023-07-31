8eeb316efd4097f7442a1ab39f245bc325e081c5
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-07-31 08:48:41 +0000

    guugulu

--- 5.fifth(DISK MANAGEMENT)/fcfs.c
+++ 5.fifth(DISK MANAGEMENT)/fcfs.c
@@ -2,14 +2,28 @@
 #include<stdlib.h>
 void main()
 {
-    int i,n,req[50],mov=0,cp;
+    int n,req[50],mov=0,cp;
     printf("Enter the current position: ");
     scanf("%d",&cp);
     printf("Enter the no of requests: ");
     scanf("%d",&n);
     printf("Enter the request order: ");
     for (int i = 0; i < n; i++)
-        scanf("%d",&cp);
+    {
+        scanf("%d",&req[i]);
+    }
     mov=mov+abs(cp-req[0]);
-    
+    printf("%d->%d",cp,req[0]);
+    for (int i = 1; i < n; i++)
+    {
+        mov=mov+abs(req[i]-req[i-1]);
+        printf("->%d",req[i]);
+    }
+    printf("\nTotal head movement = %d\n",mov);
 }
+
+// Enter the current position: 45
+// Enter the no of requests: 5
+// Enter the request order: 30 66 24 75 50
+// 45->30->66->24->75->50
+// Total head movement = 169
